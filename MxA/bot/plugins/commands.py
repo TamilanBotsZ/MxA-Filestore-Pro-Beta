b262927c8adccf2424054483755e0b946dcfcba2
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-06-28 23:01:03 +0530

    Update commands.py

--- MxA/bot/plugins/commands.py
+++ MxA/bot/plugins/commands.py
@@ -1,1 +1,11 @@
+from bot import mxabot
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+
+
+
+@mxabot.on_message(filters.command('start'))
+def start(client, message):
+  
+  
