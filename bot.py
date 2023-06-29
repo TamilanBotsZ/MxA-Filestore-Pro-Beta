ffdfae0e671a1d90421a3df0552166c6bbbf3df8
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-06-29 15:21:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 from pyrogram import Client, filters
+from pyrogram.types import Message
 import os
 from os import getenv
-from MxA.bot.plugins import commands
+#from MxA.bot.plugins import commands
 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
@@ -12,6 +13,12 @@ mxabot = Client('Mxa_Movies_Bot', api_id=API_ID, api_hash=API_HASH, bot_token=BO
 @mxabot.on_message(filters.private)
 async def hello(client, message):
     await message.reply("Hello {message.from_user.mention!!")
+
+
+@mxabot.on_message(filters.command('start'))
+def start(client, message):
+  me = client.get_me()
+  message.reply_text("Hᴇʟʟᴏ {message.from_user.mention}, I Aᴍ {me.mention}!")
   
 mxabot.add_handler(commands.handler)
 
