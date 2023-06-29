78c466f9ae7147e8df4a47065c78ce7f5f7c6931
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-06-29 14:46:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,12 +3,13 @@ import os
 from os import getenv
 from MxA.bot.plugins import commands
 
-
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 mxabot = Client('Mxa_Movies_Bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+mxabot.add_handler(commands.handler)
 
 if __name__ == "__main__":
   mxabot.run()
