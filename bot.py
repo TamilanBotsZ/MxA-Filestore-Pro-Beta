7f0197217e22d66f3a04ed294a896fb2f2d7a3bf
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-06-29 08:04:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 from pyrogram import Client, filters
 import os
 from os import getenv
+from MxA.bot.plugins import commands
+
 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
@@ -8,4 +10,6 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 mxabot = Client('Mxa_Movies_Bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-mxabot.run()
+if __name__ == "__main__":
+  mxabot.run()
+
