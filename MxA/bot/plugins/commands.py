b90a23e60716573d747f622508f9ce26e2a9d803
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-06-29 08:33:51 +0530

    Update commands.py

--- MxA/bot/plugins/commands.py
+++ MxA/bot/plugins/commands.py
@@ -7,5 +7,5 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @mxabot.on_message(filters.command('start'))
 def start(bot: Client, message: Message):
-  me = client.get_me()
+  me = bot.get_me()
   message.reply_text("Hᴇʟʟᴏ {message.from_user.mention}, I Aᴍ {me.mention}!")
