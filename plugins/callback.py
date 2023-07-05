f4c462796366dde67ffd341bf3fd6e275be35959
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-07-05 22:15:48 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -15,18 +15,9 @@ async def delete_button(bot: Client, query: CallbackQuery):
     return
 
 @mxabot.on_callback_query(filters.regex('^rfrsh$'))
-async def rfrsh_button(bot: Client, query: CallbackQuery, message: Message):
-    rfrsh_msg = await message.reply_text("Checking Please wait...")
-    if FSUB_CHANNEL:
-        try:
-            user = bot.get_chat_member(FSUB_CHANNEL, query.from_user.id)
-            if user.status == "banned":
-                await message.reply_text("Sorry you are banned 🥲")
-                await rfrsh_msg.delete()
-                return
-        except UserNotParticipant:
-            await query.answer(" Pls join channel then click on button", show_alert=True)
-            await rfrsh_msg.delete()
-            return
+async def rfrsh_button(bot: Client, query: CallbackQuery):
+    if UserNotParticipant:
+        await query.answer("Please join the channel first, then click the button", show_alert=True)
+        return
 
-        await query.answer(url="https://t.me/NG_FilestoreBot?start=start")
+    await query.answer(url="https://t.me/NG_FilestoreBot?start=start")
