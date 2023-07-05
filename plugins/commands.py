84aa05c3af0498054794d8f7bf589819c82ae15c
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-07-05 17:27:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,12 +1,38 @@
 from bot import mxabot
 from pyrogram import Client, filters
-from pyrogram.types import Message #,InlineKeyboardMarkup, InlineKeyboardButton
+#from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    CallbackQuery,
+    Message
+)
+from pyrogram.errors import QueryIdInvalid
 
-
-
+START_TEXT = "Hᴇʟʟᴏ {}, I Aᴍ MxA Pɪᴍɪᴜᴍ Fɪʟᴇsᴛᴏʀᴇ Bᴏᴛ!"
 
 @mxabot.on_message(filters.command('start'))
 def start(client, message):
  await message.delete()
  # me = self.get_me()
- message.reply_text("Hᴇʟʟᴏ {message.from_user.mention}, I Aᴍ MxA Pɪᴍɪᴜᴍ Fɪʟᴇsᴛᴏʀᴇ Bᴏᴛ!")
+ message.reply_text(
+  START_TEXT.format(cmd.from_user.mention),
+  disable_web_page_preview=True,
+  reply_markup=InlineKeyboardMarkup(
+   [
+    [
+     InlineKeyboardButton("Dev 👨‍💻", user_id="6112935306"),
+     InlineKeyboardButton("Close ❌", callback_data=f"delete")
+    ]
+   ]
+  )
+
+@mxabot.on_callback_query(filters.regex('^delete$'))
+async def delete_button(bot: Client, query: CallbackQuery):
+    await query.message.delete(true)
+ try:
+  await query.answer()
+ except QueryIdInvalid: pass
+
+
+
