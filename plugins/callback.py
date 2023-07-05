7a6971a6e5a2eaecd6ad1ab8c15ea002c80bdf25
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-07-05 20:49:30 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import (
     InlineKeyboardMarkup,
     InlineKeyboardButton,
-    Message
+    Message,
 )
 from plugins.commands import start
 from plugins.commands import FSUB_CHANNEL
@@ -13,28 +13,29 @@ async def delete_button(bot: Client, query: CallbackQuery):
     await query.message.delete(True)
     return
 
-
 @mxabot.on_callback_query(filters.regex('^rfrsh$'))
-async def delete_button(bot: Client, query: CallbackQuery):
-        rfrsh_msg = query.reply("Checking Please wait...")
+async def rfrsh_button(bot: Client, query: CallbackQuery):
+    rfrsh_msg = await query.reply("Checking Please wait...")
     if FSUB_CHANNEL:
         try:
-            user = client.get_chat_member(FSUB_CHANNEL, message.from_user.id)
+            user = bot.get_chat_member(FSUB_CHANNEL, query.from_user.id)
             if user.status == "banned":
-                cmd.reply_text("Sorry you are banned 🥲")
+                await query.reply_text("Sorry you are banned 🥲")
+                await rfrsh_msg.delete()
                 return
         except UserNotParticipant:
-            cmd.reply_text(
-                text="Hey bruh you have to subscribe my updates channel to use me",
+            await query.reply_text(
+                text="Hey bruh you have to subscribe to my updates channel to use me",
                 reply_markup=InlineKeyboardMarkup(
-                [
-                [
-                   InlineKeyboardButton("Join Channel", url=f"t.me/{FSUB_CHANNEL}")
-                ],
-                [
-                    InlineKeyboardButton("Contact Admin", url="t.me/Ng_IB_Bot")
-                ]
-            ]
-        )
-    )
+                    [
+                        [
+                            InlineKeyboardButton("Join Channel", url=f"t.me/{FSUB_CHANNEL}")
+                        ],
+                        [
+                            InlineKeyboardButton("Contact Admin", url="t.me/Ng_IB_Bot")
+                        ]
+                    ]
+                )
+            )
             return start
+    await rfrsh_msg.delete()
