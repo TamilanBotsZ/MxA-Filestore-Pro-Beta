459f4e9beda1294975da0571c86ae19dff620429
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-07-05 19:31:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -10,29 +10,27 @@ from plugins.forcesub import force_sub
 
 START_TEXT = '''Hᴇʟʟᴏ {}, I Aᴍ MxA Pɪᴍɪᴜᴍ Fɪʟᴇsᴛᴏʀᴇ Bᴏᴛ!'''
 fsub = -1001974852033
+
 @mxabot.on_message(filters.command('start'))
 async def start(client, message):
- await force_sub(client, message, fsub=fsub)
-return
- await message.delete()
- # me = self.get_me()
- await  message.reply_text(
-  START_TEXT.format(message.from_user.mention),
-  disable_web_page_preview=True,
-  reply_markup=InlineKeyboardMarkup(
-   [
-    [
-     InlineKeyboardButton("Dev 👨‍💻", user_id="6112935306"),
-     InlineKeyboardButton("Close ❌", callback_data=f"delete")
-    ]
-   ]
-  )
- )
+    await force_sub(client, message, fsub=fsub)
+    return
+    await message.delete()
+    # me = self.get_me()
+    await message.reply_text(
+        START_TEXT.format(message.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("Dev 👨‍💻", user_id="6112935306"),
+                    InlineKeyboardButton("Close ❌", callback_data=f"delete")
+                ]
+            ]
+        )
+    )
 
 @mxabot.on_callback_query(filters.regex('^delete$'))
 async def delete_button(bot: Client, query: CallbackQuery):
     await query.message.delete(True)
     return
-
-
-
