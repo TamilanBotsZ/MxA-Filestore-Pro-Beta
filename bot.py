7e73b0140a118c6542d18312d1c2731fd4e40a82
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-07-05 16:40:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,9 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 import os
 from os import getenv
-from MxA.bot.plugins import commands
+import logging
+import logging.config
+#from MxA.bot.plugins import commands
 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
