d9cef31a7a8e320fd01dc395f99fdccc0f8e966a
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-07-05 16:38:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,19 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
-mxabot = Client('Mxa_Movies_Bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+class mxabot(Client):
+
+    def __init__(self):
+        super().__init__(
+            "Mxa_Movies_Bot",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=300,
+            plugins={"root": "plugins"},
+            sleep_threshold=10,
+        )
+#Client('Mxa_Movies_Bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="plugins"))
 
 #@mxabot.on_message(filters.private)
 #async def hello(client, message):
@@ -20,8 +32,22 @@ mxabot = Client('Mxa_Movies_Bot', api_id=API_ID, api_hash=API_HASH, bot_token=BO
 #  me = client.get_me()
 #  message.reply_text("Hᴇʟʟᴏ {message.from_user.mention}, I Aᴍ {me.mention}!")
   
-mxabot.add_handler(commands.handler)
+#mxabot.add_handler(commands.handler)
+    async def start(self):
+      await super().start()
+      logging.info('Bot started')
+
+
+
+    async def stop(self, *args):
+        await super().stop()
+        me = await self.get_me()
+        logging.info(f"{me.first_name} is_...  ♻️Restarting...")
+
+
+
+
 
 if __name__ == "__main__":
-  mxabot.run()
-
+  app =mxabot
+  app.run()
