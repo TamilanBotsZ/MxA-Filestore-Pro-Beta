b5be8540695888a39f6a7cc8b398c98096959b05
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-07-07 00:06:49 +0530

    Update adduser.py

--- handlers/adduser.py
+++ handlers/adduser.py
@@ -4,11 +4,11 @@ from pyrogram import Client
 from pyrogram.types import Message
 
 
-async def add_user_in_db(bot: Client, cmd: Message):
-    if not await db.is_user_exist(cmd.from_user.id):
-        await db.add_user(cmd.from_user.id)
+async def add_user_in_db(bot: Client, message: Message):
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id)
         if Config.LOG_CHANNEL is not None:
             await bot.send_message(
                 int(Config.LOG_CHANNEL),
-                f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
+                f"#NEW_USER: \n\nNew User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) started @{Config.BOT_USERNAME} !!"
             )
