1a9921d1298628c6fef1429f1cb0dbc371afc1c4
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-07-06 22:39:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,20 +15,19 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 class mxabot(Client):
     def main():
-    plugins = dict(root="plugins")
-    app = Client("NG_FileStoreBot",
-                 bot_token=BOT_TOKEN,
-                 api_id=API_ID,
-                 api_hash=API_HASH,
-                 plugins=plugins,
-                 workers=300,
-                 sleep_threshold=10,
-                 in_memory=True
-                )
+        plugins = dict(root="plugins")
+        app = Client("NG_FileStoreBot",
+                     bot_token=BOT_TOKEN,
+                     api_id=API_ID,
+                     api_hash=API_HASH,
+                     plugins=plugins,
+                     workers=300,
+                     sleep_threshold=10,
+                     in_memory=True
+                    )
 
-    app.run()
+        app.run()
 
 
 if __name__ == "__main__":
-    main()
-
+    mxabot.main()
