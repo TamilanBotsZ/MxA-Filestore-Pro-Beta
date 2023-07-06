b8747ff43858cf62ec29a14109af0c453d5329a7
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-07-06 22:37:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,44 +1,34 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
 import os
 from os import getenv
 import logging
 import logging.config
 
+logging.getLogger().setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+from pyromod import listen
+from pyrogram import Client
 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 class mxabot(Client):
+    def main():
+    plugins = dict(root="plugins")
+    app = Client("NG_FileStoreBot",
+                 bot_token=BOT_TOKEN,
+                 api_id=API_ID,
+                 api_hash=API_HASH,
+                 plugins=plugins,
+                 workers=300,
+                 sleep_threshold=10,
+                 in_memory=True
+                )
 
-    def __init__(self):
-        super().__init__(
-            "Mxa_Movies_Bot",
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            workers=300,
-            plugins={"root": "plugins"},
-            sleep_threshold=10,
-            in_memory=True
-        )
-
-    async def start(self):
-      await super().start()
-      logging.info('Bot started')
-
-
-
-    async def stop(self, *args):
-        await super().stop()
-        me = await self.get_me()
-        logging.info(f"{me.first_name} is_...  ♻️Restarting...")
-
-
-
+    app.run()
 
 
 if __name__ == "__main__":
-  app = mxabot()
-  app.run()
+    main()
+
