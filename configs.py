081198d874ab2f5559980e71736f5b8c780bc796
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-07-07 05:45:18 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,4 +8,4 @@ class Config(object):
 	#DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
 	#BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
 	DATABASE_URL = os.environ.get("DATABASE_URL")
-        LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+        LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
