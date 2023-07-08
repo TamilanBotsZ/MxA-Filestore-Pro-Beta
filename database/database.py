b5e96504a48e28e68e40bb034cc1dda0fb6586b6
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-07-08 21:32:03 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -7,7 +7,7 @@ class Database:
 
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.db = self._client[ngfilestorebot]
+        self.db = self._client[database_name]
         self.col = self.db.users
 
 
