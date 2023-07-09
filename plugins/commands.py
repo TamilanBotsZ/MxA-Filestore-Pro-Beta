1d9e57a652212fd722cb2ebebeedec35d348b05e
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-07-09 07:39:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -10,9 +10,9 @@ from pyrogram.types import (
 from plugins.forcesub import force_sub
 
 me = mxabot()
-bot = me.get_me()
-bot_name = me.fisrst_name
-bot_id = bot.id
+bot = await me.get_me()
+bot_name = await me.fisrst_name
+bot_id = await bot.id
 START_TEXT = '''Hᴇʟʟᴏ {}, I Aᴍ [{bot_name}]tg://user?id=bot_id)'''
 
 
