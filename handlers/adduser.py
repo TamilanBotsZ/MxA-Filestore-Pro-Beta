ec770b66dca61e3b6c2eafdbdda837592b81d001
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-07-10 15:30:33 +0530

    Update adduser.py

--- handlers/adduser.py
+++ handlers/adduser.py
@@ -1,14 +1,15 @@
-from configs import Config
+from configs import *
 from database.database import db
 from pyrogram import Client
 from pyrogram.types import Message
+from bot import mxabot
 
 
-async def add_user_in_db(bot: Client, message: Message):
+async def add_user(Client, message: Message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id)
-        if Config.LOG_CHANNEL is not None:
-            await bot.send_message(
-                int(Config.LOG_CHANNEL),
-                f"#NEW_USER: \n\nNew User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) started @{Config.BOT_USERNAME} !!"
+        if LOG_CHANNEL is not None:
+            await mxabot.send_message(
+                int(LOG_CHANNEL),
+                f"#new_user :-\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) started @{BOT_USERNAME}!"
             )
